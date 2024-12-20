0223921f7fff86afb6af9c65a4fe5583a18b1250
Vishal Singh <vishalsingh173911@gmail.com>
2024-12-20 13:21:10 +0530

    text change

--- src/data/contact-data.ts
+++ src/data/contact-data.ts
@@ -1,29 +1,28 @@
 import {
-    BriefcaseIcon,
-    ChartBarIcon,
-    PlayIcon,
-  } from "@heroicons/react/24/solid";
-  
-  const contactData = [
-    {
-      title: "Excelent Services",
-      icon: BriefcaseIcon,
-      description:
-        "Quick, efficient solutions tailored to meet your specific business needs.",
-    },
-    {
-      title: "Grow Your Market",
-      icon: ChartBarIcon,
-      description:
-        "Quick, efficient strategies to expand your business reach and impact",
-    },
-    {
-      title: "Launch Time",
-      icon: PlayIcon,
-      description:
-        "Seamlessly launch your project with our expert guidance and efficient solutions.",
-    },
-  ];
-  
-  export default contactData;
-  
+  BriefcaseIcon,
+  ChartBarIcon,
+  PlayIcon,
+} from "@heroicons/react/24/solid";
+
+const contactData = [
+  {
+    title: "Our Mission",
+    icon: BriefcaseIcon,
+    description:
+      "We combine the passion and agility of a startup with the stability of an established business, positioning us perfectly to deliver exceptional professional services",
+  },
+  {
+    title: "Our Technology",
+    icon: ChartBarIcon,
+    description:
+      "Innovations and emerging technologies are evolving daily. Staying true to our agile mindset, we continuously stay updated with a wide range of new and upcoming advancements",
+  },
+  {
+    title: "Innovation Centre",
+    icon: PlayIcon,
+    description:
+      "Share your idea with us and our expert team will guide you through with best efficient solutions",
+  },
+];
+
+export default contactData;
